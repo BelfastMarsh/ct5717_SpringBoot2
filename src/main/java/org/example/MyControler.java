4ac0a62cf3ad4c403f9aabb4ba2e2e547b5eea10
Peter <peteremarsh@gmail.com>
2023-10-26 21:03:22 +0100

    added webhook test 2

--- src/main/java/org/example/MyControler.java
+++ src/main/java/org/example/MyControler.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyControler {
     @GetMapping("/")
     public String index(){
-        return "Greetings from Spring Boot, with Web Hook!";
+        return "Greetings from Spring Boot, with Web Hook!\nNow testing mid run";
     }
 }
